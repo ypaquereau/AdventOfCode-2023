bf3e3348b514c31165c269c0f8d2ff92863a724e
Yoann Paquereau <yoann.paquereau@gmail.com>
2023-12-04 22:37:11 +0100

    wip day01

--- d01/day01.php
+++ d01/day01.php
@@ -3,16 +3,32 @@
 $total = 0;
 
 foreach (file('input') as $line) {
-    preg_match_all('/\d/', $line, $matches);
+    preg_match_all('/[1-9]|one|two|three|four|five|six|seven|eight|nine/', $line, $matches);
 
     $numbers = $matches[0];
 
-    if (count($numbers) > 0) {
-        $number = (int) "{$numbers[array_key_first($numbers)]}{$numbers[array_key_last($numbers)]}";
+    $firstNumber = getNumber($numbers[array_key_first($numbers)]);
+    $lastNumber = getNumber($numbers[array_key_last($numbers)]);
 
-        $total += $number;
-    }
+    $total += (int) "$firstNumber$lastNumber";;
 }
 
 # Print 53974
-echo $total;
+echo $total . "\n";
+
+function getNumber(string $number): string
+{
+    $numbersInLetters = [
+        'one' => '1',
+        'two' => '2',
+        'three' => '3',
+        'four' => '4',
+        'five' => '5',
+        'six' => '6',
+        'seven' => '7',
+        'eight' => '8',
+        'nine' => '9',
+    ];
+
+    return $numbersInLetters[$number] ?? $number;
+}
